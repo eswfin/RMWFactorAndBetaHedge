70eff1c7e02b0c8afe5b46a7acd206d5dda24864
Xin Yi <xin.yi@anu.edu.au>
2020-10-20 01:16:51 +0800

    Finished function get_return_data().

--- Strategy.py
+++ Strategy.py
@@ -2,7 +2,9 @@
 # and beta calculation are stored
 
 import tensorflow as tf
+import pandas as pd
 import numpy as np
+import math
 
 
 # ----------------- Hyper-parameter Setting ----------------- #
@@ -15,6 +17,24 @@ REGULARIZER = 0.001
 LEARNING_RATE_BASE = 0.5
 LEARNING_RATE_DECAY = 0.95
 MOVING_AVERAGE_DECAY = 0.95
+
+
+# The method for generating return data from price data frame
+def get_return_data(price_data_frame):
+    target_name = list(price_data_frame.columns)
+    target_number = len(target_name)
+    price_data_len = len(price_data_frame)
+
+    # Start calculation. The return rate is calculated using logarithm return method.
+    return_data_len = price_data_len - 1
+    return_data_frame = pd.DataFrame(np.zeros((return_data_len, target_number)), columns=target_name)  # Init frame
+    for day in range(return_data_len):
+        price_row = day + 1
+        day_slice = []
+        for target in range(target_number):
+            day_slice[target] = math.log(price_data_frame.iloc[price_row, target] / price_data_frame.iloc[day, target])
+        return_data_frame.loc[day] = day_slice
+    return return_data_frame
 
 
 def get_initial_weight():
